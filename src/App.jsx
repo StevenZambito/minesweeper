e6604233298be0c52cfed20799a0db2e4b894a5e
StevenZambito <zambito.steven@icloud.com>
2021-02-19 16:38:13 -0500

    Fininshed CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,114 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <main>
+      <div className="button-difficulty">
+        <button>Easy</button>
+        <button>Medium</button>
+        <button>Hard</button>
+      </div>
+
+      <table className="sweeper-table">
+        <tr className="table-header">
+          <td className="mine-number" colSpan={3}>
+            10
+          </td>
+          <td className="table-header-row" align="center" colSpan={2}>
+            <button className="sweeper-button">🙂</button>
+          </td>
+          <td className="timer" colSpan={3}>
+            0
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </main>
+  )
 }
